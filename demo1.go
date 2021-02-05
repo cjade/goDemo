5f7daa04bddfd79630c5d24adac65e20e741dd00
Jade <862226994@qq.com>
2021-02-05 22:36:10 +0800

    自定义命令源码文件的参数使用说明

--- demo1.go
+++ demo1.go
@@ -3,22 +3,39 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
 )
 
-//go run demo1.go -name="te" -aa="dsd"
+//go run demo1.go --help  该命令源码文件的参数说明
 
-// 第一种获取命令行参数： key,默认值，说明
-var aa = flag.String("aa", "哈哈", "dfdf")
+//go run demo1.go -name="te" -aa="dsd" 命令就可以为参数name,aa传值
 
 var name string
 
+var aa *string
+
 func init() {
+	//自定义命令源码文件的参数使用说明,推荐使用这种方式
+	flag.CommandLine = flag.NewFlagSet("", flag.ExitOnError)
+	flag.CommandLine.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+		flag.PrintDefaults()
+	}
+	// 第一种获取命令行参数： key,默认值，说明
+	aa = flag.String("aa", "哈哈", "dfdf")
+
 	// 第二种获取命令行参数：地址，key,默认值，说明
 	flag.StringVar(&name, "name", "everyone", "The greeting object.")
+
 }
 
 func main() {
-	// 把用户传递的命令行参数解析为对应变量的值
+	// 自定义命令源码文件的参数使用说明,对flag.Usage的赋值必须在调用flag.Parse函数之前。
+	// flag.Usage = func() {
+	// 	fmt.Fprintf(os.Stderr, "Usage of %s:\n", "question")
+	// 	flag.PrintDefaults()
+	// }
+	// 函数flag.Parse用于真正解析命令参数，并把它们的值赋给相应的变量。
 	flag.Parse()
 	fmt.Println("Hello, 世界")
 	fmt.Printf("Hello, %s!\n", name)
